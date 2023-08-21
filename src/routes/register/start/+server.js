b48a9ba085e51d958ae9faa943b589b392081ac5
Hexagon Six <hexagon6@fet.li>
2023-08-21 11:38:19 +0200

    initial sveltekit skeleton

---
+++ src/routes/register/start/+server.js
@@ -0,0 +1,10 @@
+import { error } from '@sveltejs/kit'
+
+/**
+ * @param {any} event
+ */
+export function GET(event) {
+  console.log(event)
+  //   throw error(404, 'Not Found')
+  return new Response(JSON.stringify({ message: 'register start' }))
+}
