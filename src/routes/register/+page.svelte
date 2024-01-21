79f565059cc0f554cdb2d0238cd362961a7e456b
Hexagon Six <hexagon6@fet.li>
2024-01-21 15:42:14 +0100

    improve presentation, add progress meter

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -5,8 +5,10 @@
   $: registerStartResponse = undefined
   $: fido2CreateResult = undefined
   $: registerFinishResponse = undefined
+  $: progress = 0
 
   const register = async () => {
+    progress = 0
     console.log('getting public key for user', username)
     await fetch('/register/start', {
       method: 'post',
@@ -14,6 +16,7 @@
     })
       .then(async (res) => res.json())
       .then(async (res) => {
+        progress = 1
         if (res.message) {
           // FIXME: if we have an error on registration show it to the user
           console.error(res.message)
@@ -23,12 +26,14 @@
         registerStartResponse = res
         const { challenge } = res
         const data = await fido2Create(res, username).catch(console.error)
+        progress = 2
         fido2CreateResult = data
 
         if (!data) {
           console.error('no data from fido2Create.. something went wrong?')
           return
         }
+        progress = 3
         const finishBody = JSON.stringify({ ...data, challenge })
         await fetch('/register/finish', {
           method: 'post',
@@ -36,13 +41,16 @@
         })
           .then((res) => res.json())
           .then((res) => {
+            progress = 4
             if (res.message) {
               console.error(res.message)
               registerFinishResponse = res.message
               return
             }
+            progress = 5
             if (res) {
               registerFinishResponse = res
+              progress = 6
               alert('Successfully created using webAuthn')
             }
           })
@@ -54,33 +62,46 @@
   let username = ''
 </script>
 
-<form>
-  <label for="username">Username:</label>
-  <input
-    name="username"
-    id="loginform.username"
-    type="text"
-    bind:value={username}
-    autocomplete="username webauthn"
-  />
-  <button on:click={register}>register</button>
-</form>
-/register/start response:
-<textarea>{JSON.stringify(registerStartResponse, null, 2)}</textarea>
-fido2Create Result:
-<textarea>{JSON.stringify(fido2CreateResult, null, 2)}</textarea>
-/register/finish response:
-<textarea class="small"
-  >{JSON.stringify(registerFinishResponse, null, 2)}</textarea
->
+<header />
+<main>
+  <h2>User Registration with passkey</h2>
+  <form>
+    <label for="username">Username:</label>
+    <input
+      name="username"
+      id="loginform.username"
+      type="text"
+      bind:value={username}
+      autocomplete="username webauthn"
+    />
+    <button on:click={register}>register</button>
+  </form>
+  <hr />
+  <aside>
+    <label for="progress">Registration Progress:</label>
+    <meter
+      id="progress"
+      min="0"
+      max="6"
+      low="2"
+      high="4"
+      optimum="6"
+      value={progress}>at {progress}/6</meter
+    >
+  </aside>
+  <div>
+    {#each [['/register/start response:', registerStartResponse], ['fido2Create Result:', fido2CreateResult], ['/register/finish response:', registerFinishResponse]] as [heading, content]}
+      <h3>{heading}</h3>
+      <section>
+        <textarea>{JSON.stringify(content, null, 2)}</textarea>
+      </section>
+    {/each}
+  </div>
+</main>
 
 <style>
   textarea {
     width: 90%;
-    min-height: 12em;
-  }
-
-  textarea.small {
     min-height: 5em;
   }
 </style>
